b6a5689ab557ae377a83cd48c27b03cf7a9b0b1e
leonperez1870 <leo@parkfieldcommerce.com>
2019-05-20 15:41:15 -0400

    edit post data and views

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const dataDiv = document.querySelector('.js-dataGrab');
   let dataTotal = parseInt(dataDiv.dataset.total);
   let dataReceived = parseInt(dataDiv.dataset.received);
-  let dueDate = parseInt(dataDiv.dataset.dueDate);
+  let dueDate = dataDiv.dataset.dueDate;
   let stillNeeded = Math.abs(dataTotal - dataReceived);
   document.querySelector('.js-amtLeft').innerHTML = `$${stillNeeded}`;
+
+  const oneDay = 24*60*60*1000;
+  let firstDate = new Date();
+  let secondDate = new Date(dueDate);
+  firstDate = firstDate.getTime();
+  secondDate = secondDate.getTime();
+  let nDays = (secondDate - firstDate) / 1000 / 86400;
+  nDays = Math.round(nDays - 0.5);
+  if (nDays > 1) {
+    document.querySelector('.js-daysLeft').innerHTML = `Only ${nDays} days left`;
+  } else {
+    document.querySelector('.js-daysLeft').innerHTML = `Only ${nDays} day left`;
+  }
+
+  function formSerialize(formElement) {
+    const values = {};
+    const inputs = formElement.elements;
+
+    for (let i = 0; i < inputs.length; i++) {
+      values[inputs[i].name] = inputs[i].value;
+    }
+    return values;
+  }
+  const form = document.querySelector('.js-form');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const data = formSerialize(document.querySelector('.js-form'));
+    const obj = {donationAmt: 0};
+    obj.donationAmt = parseInt(data.donationAmt);
+    if (isNaN(obj.donationAmt)) {
+      alert('Please enter amount');
+      return false;
+    }
+    e.target.submit();
+  });
+
+  const url = window.location.href;
+  if (url.indexOf('?') > -1) {
+    let hasDonated = url.split('?')[1];
+    // Show a popup here!
+    console.log(hasDonated);
+  }
 });
--- routes/receive-donation.js
+++ routes/receive-donation.js
@@ -5,25 +5,23 @@ const data = JSON.parse(fs.readFileSync('./data/donationData.json', 'utf8'));
 
 router.post('/receive-donation', async (req, res) => {
   console.log('====================Receiving Donation!====================');
-  try {
-    const donationAmt = req.body.donationAmt;
-    let amtRecieved = data.amtRecieved;
-    let numOfDonors = data.numOfDonors;
-    amtRecieved = Math.abs(donationAmt + amtRecieved);
-    numOfDonors = Math.abs(numOfDonors + 1);
-    data.numOfDonors = numOfDonors;
-    data.amtRecieved = amtRecieved;
-    fs.writeFileSync('./data/donationData.json', JSON.stringify(data), 'utf8', (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log('Saved File');
-    });
-    console.log(data);
-    res.end();
-  } catch (e) {
-    res.send(e)
-  }
+  const donationAmt = parseInt(req.body.donationAmt);
+  let amtRecieved = parseInt(data.amtRecieved);
+  let numOfDonors = data.numOfDonors;
+  let hasDonated = false;
+  amtRecieved = Math.abs(donationAmt + amtRecieved);
+  numOfDonors = Math.abs(numOfDonors + 1);
+  data.numOfDonors = numOfDonors;
+  data.amtRecieved = amtRecieved;
+  fs.writeFile('./data/donationData.json', JSON.stringify(data), 'utf8', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Saved File');
+  });
+  hasDonated = true;
+  res.redirect(303, `/?hasDonated=${hasDonated}`);
+  res.end();
 });
 
 module.exports = router;
